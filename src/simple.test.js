1c82d7d69e17d474e2cdb7b6393a8d3690a433cb
aumilkyeong <aumilkyeong@gmail.com>
2020-06-17 13:38:46 +0900

    TDD 기본 개념 설명
    함수 시그니쳐는 함수명, 인자, 반환값을 의미하며 어떤 연산인지 정의하는 개념이다.
    TDD 사이클은 테스트 실패 - 통과 - 리팩토링을 반복하는 것이다.

--- src/simple.test.js
+++ src/simple.test.js
@@ -1,4 +1,11 @@
-test('simple', () => {
-  // assertion => A(actual)가 B(expect)여야 한다.
-  expect(1 + 1).toBe(2);
+function add(x, y) {
+  return x + y;
+}
+
+test('add', () => {
+  expect(add(1, 3)).toBe(4);
 });
+
+// Signature: name(add), parameters(x, y), return(result)
+
+// TDD Cycle: RED - GREEN - REFACTORING
