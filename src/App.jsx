9e70369c86f0582f0035ce0b0d9db7c86c089838
aumilkyeong <aumilkyeong@gmail.com>
2020-06-29 11:08:06 +0900

    리덕스 도입 후 투두앱 구조 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,14 @@
 import React from 'react';
 
-import { useDispatch, useSelector } from 'react-redux';
-
-import Page from './Page';
-
-import {
-  updateTaskTitle, addTask, deleteTask,
-} from './actions';
-
-function selector(state) {
-  return {
-    taskTitle: state.taskTitle,
-    tasks: state.tasks,
-  };
-}
+import ListContainer from './ListContainer';
+import InputContainer from './InputContainer';
 
 export default function App() {
-  const { taskTitle, tasks } = useSelector(selector);
-
-  const dispatch = useDispatch();
-
-  function handleClickAddTask() {
-    dispatch(addTask());
-  }
-
-  function handleChangeTitle(event) {
-    dispatch(updateTaskTitle(event.target.value));
-  }
-
-  function handleClickDeleteTask(id) {
-    dispatch(deleteTask(id));
-  }
-
   return (
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      tasks={tasks}
-      onClickAddTask={handleClickAddTask}
-      onClickDeleteTask={handleClickDeleteTask}
-    />
+    <div>
+      <h1>To-do</h1>
+      <InputContainer />
+      <ListContainer />
+    </div>
   );
 }
