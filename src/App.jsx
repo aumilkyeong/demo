76408283af47910f9987ddd807e5d9422de732f5
aumilkyeong <aumilkyeong@gmail.com>
2020-07-05 14:15:01 +0900

    5주차 앱 틀 잡기

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
+// 0: 지역, 분류 얻기
+// 1: 지역 선택 <- Regions <- API (0)
+// 2: 분류 선택 <- Categories <- API (0)
+// 3: 식당 목록 <- Restaurants <- API(with region, category ) (1,2 모두 완료된 경우)
+
+function RegionsConatiner() {
+  return null;
+}
+
+function CategoriesContainer() {
+  return null;
+}
+
+function RestaurantsContainer() {
+  return null;
+}
+
 export default function App() {
   return (
     <div>
-      <h1>Hello, World</h1>
+      <RegionsConatiner />
+      <CategoriesContainer />
+      <RestaurantsContainer />
     </div>
   );
 }
