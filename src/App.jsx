a8084d52bcb3917c6a86451ea9064b1f1dc689dd
aumilkyeong <aumilkyeong@gmail.com>
2020-07-05 14:08:38 +0900

    5주차 풀이전 프로젝트 초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,9 @@
-import React, { useEffect } from 'react';
-
-import { useDispatch } from 'react-redux';
-
-import CategoriesContainer from './CategoriesContainer';
-import RestaurantsContainer from './RestaurantsContainer';
-import RestaurantCreateContainer from './RestaurantCreateContainer';
-
-import {
-  loadRestaurants,
-  loadCategories,
-} from './actions';
+import React from 'react';
 
 export default function App() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(loadCategories());
-    dispatch(loadRestaurants());
-  }, []);
   return (
     <div>
-      <h1>Restaurants</h1>
-      <CategoriesContainer />
-      <RestaurantsContainer />
-      <RestaurantCreateContainer />
+      <h1>Hello, World</h1>
     </div>
   );
 }
